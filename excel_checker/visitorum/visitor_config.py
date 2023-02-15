f99173e2a80dd6d6a59d5e294534dcbd47214e9d
Valerio <you@example.com>
2023-02-15 12:14:24 +0100

    refactor visitor config

--- excel_checker/visitorum/visitor_config.py
+++ excel_checker/visitorum/visitor_config.py
@@ -10,26 +10,22 @@ def create_tree_structure(file, configFile):
     excel_checker = ExcelChecker(file)
     wkb = excel_checker.wrkbk
     wkb = CWorkbook(wkb)
-    if "Sheets" in data:
-        for sheet in data["Sheets"]:
-            ws = wkb.addSheet(sheet["name"])
-            if ws is None:
-                continue
-            if "Rows" in sheet:
-                for row in sheet["Rows"]:
-                    for i in range(row["from"], row["to"]+1):
-                        ws.addRow(i)
-            if "Columns" in sheet:
-                for col in sheet["Columns"]:
-                    for j in range(col["from"], col["to"]+1):
-                        ws.addCol(j)
-            if "Cells" in sheet:
-                for cell in sheet["Cells"]:
-                    result = re.search(r"^\((\d+),(\d+)\)$", cell["cell"])
-                    if result is not None:
-                        i = int(result.group(1))
-                        j = int(result.group(2))
-                        ws.addCell(i,j)
+    for sheet in data.get("Sheets", []):
+        ws = wkb.addSheet(sheet["name"])
+        if ws is None:
+            continue
+        for row in sheet.get("Rows", []):
+            for i in range(row["from"], row["to"]+1):
+                ws.addRow(i)
+        for col in sheet.get("Columns", []):
+            for j in range(col["from"], col["to"]+1):
+                ws.addCol(j)
+        for cell in sheet.get("Cells", []):
+            result = re.search(r"^\((\d+),(\d+)\)$", cell["cell"])
+            if result is not None:
+                i = int(result.group(1))
+                j = int(result.group(2))
+                ws.addCell(i,j)
     return wkb
 
 def load_checks(configFile):
@@ -43,33 +39,32 @@ def load_checks(configFile):
         data = yaml.load(yamlfile, Loader=yaml.FullLoader)
     if "Workbook" in data:
         wbc = data["Workbook"].get("checks", [])
-    if "Sheets" in data:
-        for sheet in data["Sheets"]:
-            sname = sheet["name"]
-            wsc[sname] = sheet.get("checks", [])
-            if "Rows" in sheet:
-                rc[sname] = {}
-                for row in sheet["Rows"]:
-                    for i in range(row["from"], row["to"]+1):
-                        val = rc[sname].get(i, [])
-                        rc[sname][i] = val
-                        rc[sname][i].extend(row["checks"])
-            if "Columns" in sheet:
-                cc[sname] = {}
-                for col in sheet["Columns"]:
-                    for j in range(col["from"], col["to"]+1):
-                        val = cc[sname].get(j, [])
-                        cc[sname][j] = val
-                        cc[sname][j].extend(col["checks"])
-            if "Cells" in sheet:
-                cellc[sname] = {}
-                for cell in sheet["Cells"]:
-                    result = re.search(r"^\((\d+),(\d+)\)$", cell["cell"])
-                    if result is not None:
-                        i = result.group(1)
-                        j = result.group(2)
-                        val = cellc[sname].get(cell["cell"], [])
-                        cellc[sname][cell["cell"]] = val
-                        cellc[sname][cell["cell"]].extend(cell["checks"])
+    for sheet in data.get("Sheets", []):
+        sname = sheet["name"]
+        wsc[sname] = sheet.get("checks", [])
+        if "Rows" in sheet:
+            rc[sname] = {}
+            for row in sheet["Rows"]:
+                for i in range(row["from"], row["to"]+1):
+                    val = rc[sname].get(i, [])
+                    rc[sname][i] = val
+                    rc[sname][i].extend(row["checks"])
+        if "Columns" in sheet:
+            cc[sname] = {}
+            for col in sheet["Columns"]:
+                for j in range(col["from"], col["to"]+1):
+                    val = cc[sname].get(j, [])
+                    cc[sname][j] = val
+                    cc[sname][j].extend(col["checks"])
+        if "Cells" in sheet:
+            cellc[sname] = {}
+            for cell in sheet["Cells"]:
+                result = re.search(r"^\((\d+),(\d+)\)$", cell["cell"])
+                if result is not None:
+                    i = result.group(1)
+                    j = result.group(2)
+                    val = cellc[sname].get(cell["cell"], [])
+                    cellc[sname][cell["cell"]] = val
+                    cellc[sname][cell["cell"]].extend(cell["checks"])
     return wbc, wsc, rc, cc, cellc
 
